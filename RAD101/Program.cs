ef04ba01f3a40a72217c27ef3a8c215708c7152b
S00146154 <s00146154@mail.itsligo.ie>
2017-10-27 10:47:57 +0100

    Made the tests

--- RAD101/Program.cs
+++ RAD101/Program.cs
@@ -19,11 +19,10 @@ namespace RAD101
         {
             return ID.ToString() + " " + Name + " " + Xp.ToString();
         }
-    }
 
-    class Program
-    {
-        static List<Player> players = new List<Player>()
+        class Program
+        {
+            static List<Player> players = new List<Player>()
             {
                 new Player ( ID = Guid.NewGuid(),  Name = "Pete Vloga", Xp=100),
                 new Player ( ID = Guid.NewGuid(),  Name = "John Vloga", Xp=10),
@@ -31,63 +30,64 @@ namespace RAD101
                 new Player ( ID = Guid.NewGuid(),  Name = "Ian Vloga", Xp=120),
             };
 
-        static void Main(string[] args)
-        {
-            #region Test0
-            // return the first occurance of some records
-            Player found = players.FirstOrDefault(p => p.Name == ("John"));
-
-            if (found != null)
+            static void Main(string[] args)
             {
-                Console.WriteLine("{0}", found.ToString());
-            }
-            else
-            {
-                Console.WriteLine("Not Found");
-            }
-            #endregion
+                #region Test0
+                // return the first occurance of some records
+                Player found = players.FirstOrDefault(p => p.Name == ("John"));
 
-            #region Test1
-            Player found1 = players.FirstOrDefault(p => p.Name.Contains("Pete"));
-
-            if (found != null)
-            {
-                Console.WriteLine("{0}", found.ToString());
-            }
-            else
-            {
-                Console.WriteLine("Not Found");
-            }
-            #endregion
-
-            List<Player> topPlayers = topPlayers = players.Where(plr => plr.Xp >= 100).ToList();
-
-            if (topPlayers.Count > 0)
-            {
-                foreach (var item in topPlayers)
+                if (found != null)
                 {
-                    Console.WriteLine("{0}", item.ToString());
+                    Console.WriteLine("{0}", found.ToString());
                 }
-            }
-            else
-            {
-                Console.WriteLine("Not Found");
-            }
+                else
+                {
+                    Console.WriteLine("Not Found");
+                }
+                #endregion
 
-            Console.WriteLine();
-            // scoreboard is type IEnumerable
-            var Scoreboard = players
-                .OrderByDescending(o => o.Xp)
-                // Dynamic collection / Object collection
-                .Select(scores => new { scores.Name, scores.Xp });
+                #region Test1
+                Player found1 = players.FirstOrDefault(p => p.Name.Contains("Pete"));
 
-            foreach (var item in Scoreboard)
-            {
-                Console.WriteLine("{0} {1}", item.Name, item.Xp);
+                if (found != null)
+                {
+                    Console.WriteLine("{0}", found.ToString());
+                }
+                else
+                {
+                    Console.WriteLine("Not Found");
+                }
+                #endregion
+
+                List<Player> topPlayers = topPlayers = players.Where(plr => plr.Xp >= 100).ToList();
+
+                if (topPlayers.Count > 0)
+                {
+                    foreach (var item in topPlayers)
+                    {
+                        Console.WriteLine("{0}", item.ToString());
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Not Found");
+                }
+
+                Console.WriteLine();
+                // scoreboard is type IEnumerable
+                var Scoreboard = players
+                    .OrderByDescending(o => o.Xp)
+                    // Dynamic collection / Object collection
+                    .Select(scores => new { scores.Name, scores.Xp });
+
+                foreach (var item in Scoreboard)
+                {
+                    Console.WriteLine("{0} {1}", item.Name, item.Xp);
+                }
+
+                Console.ReadKey();
+
             }
-
-            Console.ReadKey();
-        
         }
     }
 }
